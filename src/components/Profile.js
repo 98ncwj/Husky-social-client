b30bb6c42f95ec5f0743ad02a225100f0413768f
98ncwj <98nicholascheong@gmail.com>
2020-08-26 01:30:47 +1000

    first commit. Like, Unlike,Login,Logout,Signup,Delete functional

---
+++ src/components/Profile.js
@@ -0,0 +1,163 @@
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+import dayjs from "dayjs";
+import { Link } from "react-router-dom";
+import EditDetails from "./EditDetails";
+import MyButton from "../util/MyButton";
+
+//redux
+import { connect } from "react-redux";
+import { logoutUser, uploadImage } from "../redux/actions/userActions";
+
+//mui
+import Button from "@material-ui/core/Button";
+import MuiLink from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+
+//icons
+import LocationOn from "@material-ui/icons/LocationOn";
+import LinkIcon from "@material-ui/icons/Link";
+import CalendarToday from "@material-ui/icons/CalendarToday";
+import EditIcon from "@material-ui/icons/Edit";
+import KeyboardReturn from "@material-ui/icons/KeyboardReturn";
+
+const styles = (theme) => ({
+  ...theme.spreadThis,
+});
+
+class Profile extends Component {
+  handleImageChange = (event) => {
+    const image = event.target.files[0];
+    const formData = new FormData();
+    formData.append("image", image, image.name);
+    this.props.uploadImage(formData);
+  };
+
+  handleEditPicture = () => {
+    const fileInput = document.getElementById("imageInput");
+    fileInput.click();
+  };
+
+  handleLogout = () => {
+    this.props.logoutUser();
+  };
+  render() {
+    const {
+      classes,
+      user: {
+        credentials: { handle, createdAt, imgUrl, bio, website, location },
+        loading,
+        authenticated,
+      },
+    } = this.props;
+
+    let profileMarkup = !loading ? (
+      authenticated ? (
+        <Paper className={classes.paper}>
+          <div className={classes.profile}>
+            <div className="image-wrapper">
+              <img className="profile-image" src={imgUrl} alt="profile" />
+              <input
+                type="file"
+                hidden="hidden"
+                id="imageInput"
+                onChange={this.handleImageChange}
+              />
+
+              <MyButton
+                tip="Edit profile picture"
+                onClick={this.handleEditPicture}
+                btnClassName="button"
+              >
+                <EditIcon color="primary" />
+              </MyButton>
+            </div>
+            <hr />
+            <div className="profile-details">
+              <MuiLink
+                component={Link}
+                to={`/users/${handle}`}
+                color="primary"
+                variant="h5"
+              >
+                @{handle}
+              </MuiLink>
+              <hr />
+              {bio && <Typography variant="body2">{bio}</Typography>}
+              <hr />
+              {location && (
+                <Fragment>
+                  <LocationOn color="primary" /> <span>{location}</span>
+                  <hr />
+                </Fragment>
+              )}
+              {website && (
+                <Fragment>
+                  <LinkIcon color="primary" />
+                  <a href={website} target="_blank" rel="noopener noreferrer">
+                    {" "}
+                    {website}
+                  </a>
+                  <hr />
+                </Fragment>
+              )}
+              <CalendarToday color="primary" />{" "}
+              <span>Joined {dayjs(createdAt).format("MMM YYYY")}</span>
+            </div>
+            <MyButton tip="logout" onClick={this.handleLogout}>
+              <KeyboardReturn color="primary" />
+            </MyButton>
+            <EditDetails />
+          </div>
+        </Paper>
+      ) : (
+        <Paper className={classes.paper}>
+          <Typography variant="body2" aligh="center">
+            No profile found, please Log In again
+          </Typography>
+          <div className={classes.buttons}>
+            <Button
+              variant="contained"
+              color="primary"
+              component={Link}
+              to="/login"
+            >
+              Login
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              component={Link}
+              to="/signup"
+            >
+              Signup
+            </Button>
+          </div>
+        </Paper>
+      )
+    ) : (
+      <p>loading...</p>
+    );
+
+    return profileMarkup;
+  }
+}
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+Profile.protoTypes = {
+  user: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+  logoutUser: PropTypes.func.isRequired,
+  uploadImage: PropTypes.func.isRequired,
+};
+
+const mapActionsToProps = { logoutUser, uploadImage };
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(withStyles(styles)(Profile));
